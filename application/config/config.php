37714f269a59c07c002c377b0ed1869301fc8726
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2024-04-23 07:55:19 +0530

    updated

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/md_demo/';
+$config['base_url'] = 'https://project.akriti.online/';
 
 /*
 |--------------------------------------------------------------------------
