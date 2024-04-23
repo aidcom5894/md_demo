74a10484024c4426516f46318959a2cdd35f1309
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2024-04-23 10:08:24 +0530

    updated login from header section

--- application/views/developerLogin.php
+++ application/views/developerLogin.php
@@ -8,7 +8,7 @@ $this->load->view('master_page/ui_header');
 <div id="wrap-body">
 <div class="chunk">
 
-<form action="#" method="post" id="login" data-focus="username">
+<form method="POST" action="<?php echo base_url('user_login'); ?>">
 <div class="panel">
 <div class="inner">
 
@@ -16,11 +16,11 @@ $this->load->view('master_page/ui_header');
 <fieldset class="fields1">
 <dl>
 <dt><label for="username">Username:</label></dt>
-<dd><input type="text" tabindex="1" name="username" id="username" size="25" value="" class="inputbox autowidth" /></dd>
+<dd><input type="text" tabindex="1" name="portalusername" id="username" size="25" value="" class="inputbox autowidth" /></dd>
 </dl>
 <dl>
 <dt><label for="password">Password:</label></dt>
-<dd><input type="password" tabindex="2" id="password" name="password" size="25" class="inputbox autowidth" autocomplete="off" /></dd>
+<dd><input type="password" tabindex="2" id="password" name="portalpassword" size="25" class="inputbox autowidth" autocomplete="off" /></dd>
 </dl>
 <dl>
 <dd><label for="autologin"><input type="checkbox" name="autologin" id="autologin" tabindex="4" /> Remember me</label></dd>					<dd><label for="viewonline"><input type="checkbox" name="viewonline" id="viewonline" tabindex="5" /> Hide my online status this session</label></dd>
@@ -35,7 +35,8 @@ $this->load->view('master_page/ui_header');
 <dt>&nbsp;</dt>
 <dd><input type="hidden" name="sid" value="93c5ab625063b5516eb6e55d1a33d83d" />
 <input type="hidden" name="redirect" value="index.php?style=3" />
-<input type="submit" name="login" tabindex="6" value="Login" class="button1" /></dd>
+
+<input type="submit" tabindex="5" name="login" value="Login" class="button1" />	</dd>
 </dl>
 </fieldset>
 </div>
